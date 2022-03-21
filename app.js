e16b74319683f197f0b6b1f2f9aeee0030be0a65
pooreumu <pooreum83@gmail.com>
2022-03-21 17:07:36 +0900

    수정, 삭제 추가

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const ejs = require("ejs");
+const Articles = require("./schemas/article");
 const connect = require("./schemas");
 
 connect();
 
 const app = express();
 const port = 3000;
-
-app.engine('ejs', ejs.renderFile);
-
 
 
 const articleRouter = require("./routes/articles");
@@ -18,18 +16,36 @@ const request_middleware = (req, res, next) => {
     next();
 }
 
-app.use(express.json())
 app.use(request_middleware)
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
+app.engine('ejs', ejs.renderFile);
 app.use("/api", articleRouter);
 
 app.get("/", (req, res) => {
     res.render("jo.ejs")
 });
 
+app.get("/detail/:articlesId", async (req, res) => {
+    const { articlesId } = req.params
+    const articles = await Articles.findOne({ articlesId })
+    res.render("jasehi.ejs", { articles: articles })
+});
+
 app.get("/write", (req, res) => {
     res.render("jacksung.ejs")
-})
+});
+
+app.get("/revise/:articlesId", async (req, res) => {
+    res.render("revise.ejs")
+});
+
+// app.get("/revise/:articlesId", async (req, res) => {
+//     const { articlesId } = req.params
+//     const articles = await Articles.findOne({ articlesId })
+//     res.render("revise.ejs", { articles: articles })
+// });
 
 app.listen(port, () => {
     console.log(port, "포트로 서버가 켜졌어요!")
-})
+});
