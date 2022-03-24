bd22936330af82eadcbda7ab6b8075c835d5e1b9
pooreumu <pooreum83@gmail.com>
2022-03-24 10:25:30 +0900

    git ignore 수정

---
+++ schemas/index.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const connect = () => {
+    const mongoId = process.env.MONGO_ID
+    const mongoPw = process.env.MONGO_PW
+    mongoose.connect(`mongodb://${mongoId}:${mongoPw}@13.125.241.36:27017/spa_mall?authSource=admin&authMechanism=SCRAM-SHA-1`, { ignoreUndefined: true }).catch((err) => {
+        console.error(err)
+    })
+}
+
+module.exports = connect;
