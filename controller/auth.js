ff0e60a412922c3279a5bf92532cc5b97e89ee0a
pooreumu <pooreum83@gmail.com>
2022-04-10 18:51:35 +0900

    console.log 삭제, 리드미에서 도메인삭제

--- controller/auth.js
+++ controller/auth.js
@@ -74,4 +74,60 @@ async function auth(req, res) {
     },
   });
 }
-module.exports = { sign_up, sign_in, auth };
+
+async function sign_in_test(req, res) {
+  try {
+    const { nickname, password } = req.body;
+
+    const user = await User.findOne({ where: { nickname, password } });
+
+    const token_key = process.env.JWT_KEY;
+    const token = jwt.sign({ userId: user.userId }, token_key);
+
+    res.send({ token });
+  } catch (err) {
+    res.status(401).send({
+      errorMessage: "닉네임 또는 패스워드를 확인해주세요",
+    });
+    return;
+  }
+}
+
+async function sign_up_test(req, res) {
+  try {
+    const { nickname, password, confirmPassword } = await UsersSchema.validateAsync(req.body);
+
+    if (password !== confirmPassword) {
+      res.status(400).send({
+        errorMessage: "패스워드가 패스워드 확인란과 동일하지 않습니다.",
+      });
+      return;
+    }
+    // const existUsers = await User.findAll({
+    //   where: {
+    //     [Op.or]: [{ nickname }],
+    //   },
+    // });
+    // if (existUsers.length) {
+    //   res.status(400).send({
+    //     errorMessage: "중복된 닉네임입니다.",
+    //   });
+    //   return;
+    // }
+    // if (password.indexOf(nickname) !== -1) {
+    //   res.status(400).send({
+    //     errorMessage: "사용할 수 없는 암호입니다",
+    //   });
+    //   return;
+    // }
+    await User.create({ nickname, password });
+
+    res.status(201).send({});
+  } catch (err) {
+    res.status(400).send({
+      errorMessage: "닉네임 영어나 숫자만 3자이상, 암호4자이상",
+    });
+  }
+}
+
+module.exports = { sign_up, sign_in, auth, sign_in_test, sign_up_test };
